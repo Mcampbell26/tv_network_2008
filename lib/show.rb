399524759f0985e51bb67e2445903675d44d6e19
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-01 10:46:31 -0600

    Add highest_paid_actor method and test

--- lib/show.rb
+++ lib/show.rb
@@ -15,4 +15,13 @@ class Show
     salaries.sum
   end
 
+  def highest_paid_actor
+    # highest_pay = []
+    @characters.max_by do |char|
+      char.salary
+      return char.actor
+    end
+
+
+  end
 end
